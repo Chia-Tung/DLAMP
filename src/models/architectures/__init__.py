51fd4649ce79a0abf2e358c05d1665edf2a992b3
Jeffrey from neo171 <zhangjiadongniub@gmail.com>
2024-06-24 20:46:55 +0800

    WIP: Res Attn UNet

--- src/models/architectures/__init__.py
+++ src/models/architectures/__init__.py
@@ -1,3 +1,4 @@
 from .drop_path import *
 from .pangu_model import *
 from .multilayer_perceptron import *
+from .unet import *
--- src/models/builders/glide_builder.py
+++ src/models/builders/glide_builder.py
@@ -1,8 +1,60 @@
+from pathlib import Path
+import torch.nn as nn
+
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+
+from ...const import CHECKPOINT_DIR
+from ...utils import convert_hydra_dir_to_timestamp
 from .base_builder import BaseBuilder
 
 __all__ = ["GlideBuilder"]
 
 
 class GlideBuilder(BaseBuilder):
-    def __init__(self):
-        pass
+    def __init__(self, hydra_dir: Path, **kwargs):
+        super().__init__(**kwargs)
+
+        self.time_stamp = convert_hydra_dir_to_timestamp(hydra_dir)
+
+        self.info_log(f"Input Image Shape: {self.kwargs.image_shape}")
+        self.info_log(f"Patch Size: {self.kwargs.patch_size}")
+        self.info_log(f"Window Size: {self.kwargs.window_size}")
+
+    def _backbone_model(self) -> nn.Module:
+        return PanguModel(
+            image_shape=self.kwargs.image_shape,
+            patch_size=self.kwargs.patch_size,
+            window_size=self.kwargs.window_size,
+            upper_levels=len(self.pressure_levels),
+            upper_channels=len(self.upper_vars),
+            surface_channels=len(self.surface_vars),
+            embed_dim=self.kwargs.embed_dim,
+            heads=self.kwargs.heads,
+            depths=self.kwargs.depths,
+            max_drop_path_ratio=self.kwargs.max_drop_path_ratio,
+            dropout_rate=self.kwargs.dropout_rate,
+            smoothing_kernel_size=self.kwargs.smoothing_kernel_size,
+            segmented_smooth_boundary_width=self.kwargs.segmented_smooth_boundary_width,
+        )
+
+    def checkpoint_callback(self) -> ModelCheckpoint:
+        return ModelCheckpoint(
+            dirpath=CHECKPOINT_DIR,
+            filename=self.kwargs.model_name
+            + f"_{self.time_stamp}"
+            + "-{epoch:03d}-{val_loss_epoch:.4f}",
+            save_top_k=1,
+            verbose=True,
+            monitor="val_loss_epoch",
+            mode="min",
+        )
+
+    def wandb_logger(self, save_dir: str = "./logs") -> WandbLogger:
+        Path(save_dir).mkdir(parents=True, exist_ok=True)
+        return WandbLogger(
+            save_dir=save_dir,
+            log_model=False,  # log W&B artifacts
+            project="my-burdensome-project",
+            name=self.kwargs.model_name + f"_{self.time_stamp}",
+        )
