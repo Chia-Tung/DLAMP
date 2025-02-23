6a57446ba6a0d7bf4c474f7591f3bdf43d501fc5
Chia-Tung Chang <zhangjiadongniub@gmail.com>
2025-02-23 16:48:11 +0800

    Modifications due to the HPC environment
    [debug] predict by ckpt
    
    [debug] log normal standardization of Qw
    
    [feat] viz_omega and viz_swdown
    
    [feat] Add Residual Connection in Pangu Model
    
    [debug] deterministic ckpt inference
    
    Modifications due to the HPC environment

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#PJM -L rscunit=rscunit_pg01            # Resource unit name
+#PJM -L rscgrp=gpu-rd-large             # Resource group name
+#PJM -L node=1                          # GPU node
+#PJM -L vnode=1                         # number of Nodes
+#PJM -L vnode-core=32                   # CPU cores
+#PJM --mpi proc=32                      # MPI processes
+#PJM -L gpu=8                           # GPU Cards
+#PJM -L elapse=240:00:00                # elapse time
+#PJM -o 'sys_log/test.%j.out'           # write log out to log directory
+#PJM -e 'sys_log/test.%j.err'           # write error message to log directory
+
+# activate virtual env
+. /nwpr/wfc/com134/.bashrc
+
+module use /package/x86_64/nvidia/hpc_sdk/23.1/modulefiles
+module load nvhpc/23.1
+module list
+
+CUDA_PATH=/package/x86_64/nvidia/hpc_sdk/23.1/Linux_x86_64/23.1/cuda/11.8
+export PATH=${CUDA_PATH}/bin:${PATH}
+export LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH}
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=${CUDA_PATH}
+
+# Print CUDA library path
+echo "CUDA Library Path:"
+echo $LD_LIBRARY_PATH
+echo -e "\nGPU Server Information:"
+hostname
+echo -e "\nGPU Status:"
+nvidia-smi
+echo -e "\ntest python modules"
+python -V
+python -c "import torch; print(f'pytorch {torch.__version__}'); import lightning; print(f'lightning {lightning.__version__}')"
+
+# job run
+python train.py
--- unit_test.py
+++ unit_test.py
@@ -1,37 +1,27 @@
-import logging
 from pathlib import Path
 
 import hydra
 import torch
 
 torch.set_float32_matmul_precision("medium")
-from lightning.pytorch import seed_everything
 from omegaconf import DictConfig, OmegaConf
 
 from src.managers import DataManager
 from src.models import get_builder
 from src.utils import DataCompose
 
-log = logging.getLogger(__name__)
-
 
 @hydra.main(version_base=None, config_path="config", config_name="train_pangu")
 def main(cfg: DictConfig) -> None:
-    hydra_oup_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
-    log.info(f"Working directory: {Path.cwd()}")
-    log.info(f"Output directory: {hydra_oup_dir}")
-
-    # lightning ddp strategy doesn't need manual seed
-    # https://github.com/Lightning-AI/pytorch-lightning/issues/12986
-    # seed_everything(1000)
-
-    # prevent access to non-existing keys
+    # prevent access to non-existing key
     OmegaConf.set_struct(cfg, True)
+    hydra_oup_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir)
 
     # prepare data
     data_list = DataCompose.from_config(cfg.data.train_data)
     data_manager = DataManager(data_list, **cfg.data, **cfg.lightning)
     data_manager.setup("test")
+    data_manager.setup("fit")
 
     # model
     model_builder = get_builder(cfg.model.model_name)(
@@ -44,17 +34,18 @@ def main(cfg: DictConfig) -> None:
     )
     model = model_builder.build_model(data_manager.test_dataloader())
 
-    # trainer
-    wandb_logger = model_builder.wandb_logger()
-    wandb_logger.watch(model, log="all")
-    trainer = model_builder.build_trainer(wandb_logger)
+    # inference
+    data_loader = data_manager.train_dataloader()
+    inp_data, _ = next(iter(data_loader))
+    inp_data["upper_air"] = inp_data["upper_air"].cuda()
+    inp_data["surface"] = inp_data["surface"].cuda()
+    print(inp_data["upper_air"].shape, inp_data["surface"].shape)
 
-    # start training
-    trainer.fit(
-        model,
-        data_manager,
-        ckpt_path=getattr(cfg.lightning, "resume_from_checkpoint", None),
-    )
+    model = model_builder._backbone_model()
+    model = model.cuda()
+    with torch.no_grad():
+        out = model(inp_data["upper_air"], inp_data["surface"])
+    print(out[0].shape, out[1].shape)
 
 
 if __name__ == "__main__":
